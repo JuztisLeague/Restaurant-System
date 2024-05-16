d771be59182566dcde0456fc8a040a3207829834
mis2gan <167780395+mis2gan@users.noreply.github.com>
2024-05-16 21:02:38 +0800

    Add files via upload

---
+++ bon_appetit (3).sql
@@ -0,0 +1,269 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 16, 2024 at 03:00 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bon_appetit`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `accounts`
+--
+
+CREATE TABLE `accounts` (
+  `id` int(11) NOT NULL,
+  `role` varchar(256) NOT NULL,
+  `password` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `accounts`
+--
+
+INSERT INTO `accounts` (`id`, `role`, `password`) VALUES
+(1, 'Admin', 45312),
+(2, 'Chef', 54321),
+(3, 'Cashier', 22113);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appetizer`
+--
+
+CREATE TABLE `appetizer` (
+  `id` int(11) NOT NULL,
+  `dish_name` varchar(256) NOT NULL,
+  `description` varchar(256) NOT NULL,
+  `price` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `appetizer`
+--
+
+INSERT INTO `appetizer` (`id`, `dish_name`, `description`, `price`) VALUES
+(2, 'vince', 'gwapo', 99999),
+(3, 'awdad', 'awda', 21),
+(4, 'aw', 'awd', 23);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customerorderlist`
+--
+
+CREATE TABLE `customerorderlist` (
+  `customerCode` int(11) NOT NULL,
+  `customerOrderList` longtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `customerorderlist`
+--
+
+INSERT INTO `customerorderlist` (`customerCode`, `customerOrderList`) VALUES
+(43215, '3pcs Chicken'),
+(65432, '3pcs Chicken, 2pcs Burgersteak'),
+(10909, '24 Burgers'),
+(69083, '2 Fried Chickens\n2 Tempura\n1 Burger\n3 Siomai\n2 Rice'),
+(41502, '3 Siopao\n5 Lumpia\n2 Siomai'),
+(60386, '3 Chicken Meal\n20 Burger Steak'),
+(61364, ''),
+(68418, '3 BURGERS'),
+(41629, '3 Burger\n2 Ice Tea\n1 Steam Rice');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dessert`
+--
+
+CREATE TABLE `dessert` (
+  `id` int(11) NOT NULL,
+  `dish_name` varchar(256) NOT NULL,
+  `description` varchar(256) NOT NULL,
+  `price` float(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `drinks`
+--
+
+CREATE TABLE `drinks` (
+  `id` int(11) NOT NULL,
+  `dish_name` varchar(256) NOT NULL,
+  `description` varchar(256) NOT NULL,
+  `price` float(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `entree`
+--
+
+CREATE TABLE `entree` (
+  `id` int(11) NOT NULL,
+  `dish_name` varchar(256) NOT NULL,
+  `description` varchar(256) NOT NULL,
+  `price` float(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `entree`
+--
+
+INSERT INTO `entree` (`id`, `dish_name`, `description`, `price`) VALUES
+(1, 'wow', 'very delicous', 56.00);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_cart`
+--
+
+CREATE TABLE `order_cart` (
+  `id` int(11) NOT NULL,
+  `dish_name` varchar(256) NOT NULL,
+  `price` float(10,2) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `order_number` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `order_cart`
+--
+
+INSERT INTO `order_cart` (`id`, `dish_name`, `price`, `quantity`, `order_number`) VALUES
+(1, 'vince', 99999.00, 1, 39136),
+(2, 'awdad', 21.00, 1, 39136),
+(3, 'aw', 23.00, 1, 39136),
+(4, 'aw', 23.00, 1, 39136),
+(5, 'awdad', 21.00, 1, 39136);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `test`
+--
+
+CREATE TABLE `test` (
+  `name` text NOT NULL,
+  `username` varchar(10) NOT NULL,
+  `id` int(11) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `test`
+--
+
+INSERT INTO `test` (`name`, `username`, `id`, `time`) VALUES
+('ashutosh', '029', 123, '2024-04-21 12:37:23'),
+('kumar', '92', 456, '2024-04-21 12:37:46'),
+('kumar', '92', 456, '2024-04-21 12:37:58');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `accounts`
+--
+ALTER TABLE `accounts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `appetizer`
+--
+ALTER TABLE `appetizer`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `dessert`
+--
+ALTER TABLE `dessert`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `drinks`
+--
+ALTER TABLE `drinks`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `entree`
+--
+ALTER TABLE `entree`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `order_cart`
+--
+ALTER TABLE `order_cart`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `accounts`
+--
+ALTER TABLE `accounts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `appetizer`
+--
+ALTER TABLE `appetizer`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `dessert`
+--
+ALTER TABLE `dessert`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `drinks`
+--
+ALTER TABLE `drinks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `entree`
+--
+ALTER TABLE `entree`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `order_cart`
+--
+ALTER TABLE `order_cart`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
